0840b1b53fa0d46d1833e079c5d10acf9218e7ce
joao <joao.pdesa@hotmail.com>
2024-01-02 08:06:33 -0300

    feat: first commit

---
+++ resources/views/pages/login.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h1>Login</h1>
+</x-app-layout>
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::middleware('guest')->group(function() {
+    Route::get('/login', function() {
+        return view('pages.login');
+    })->name('login');
+
+    Route::get('/registro', function() {
+        return view('pages.register');
+    })->name('register');
+});
+
+Route::middleware('auth')->get('/', function () {
+    return view('pages.index');
+});
